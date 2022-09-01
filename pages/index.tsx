b3f8b10f8d8a32e8cf03c429b5816fba91be5dc2
leyuri <leyuri97@gmail.com>
2022-09-01 23:29:18 +0900

    Add : data fetch from mongodb (delete dummy data)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,8 @@
+import { MongoClient } from "mongodb";
+import { username, password } from "../config/mongodb";
+
 import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
-
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "A First Meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg/1280px-La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg",
-    address: "Some address 5, 1234 Some City",
-    description: "This is a first meetup!",
-  },
-  {
-    id: "m2",
-    title: "A Second Meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Paris%2C_France.jpg/1024px-Paris%2C_France.jpg",
-    address: "Some address 5, 1234 Some City",
-    description: "This is a second meetup!",
-  },
-];
 
 function HomePage(props: any) {
   return <MeetupList meetups={props.meetups} />;
@@ -26,9 +10,22 @@ function HomePage(props: any) {
 
 export async function getStaticProps() {
   // fetch data from an API
+  const client = await MongoClient.connect(
+    `mongodb+srv://${username}:${password}@cluster0.86k80sc.mongodb.net/meetups?retryWrites=true&w=majority`
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+  const meetups = await meetupsCollection.find().toArray();
+  client.close();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
     },
     revalidate: 1,
   };
